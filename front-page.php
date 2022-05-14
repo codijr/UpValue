29cee85f4fdc5a0f9a9b521a07c853767cfa5cac
kingnaldoo <reinaldosn2016@gmail.com>
2022-05-14 12:35:15 -0300

    fix banner width

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <main>
     <section class="d-flex flex-column justify-content-center" id="banner">
         <div class="container">
-            <div class="w-50">
+            <div class="w-100">
                 <h1 class="mb-2">Potencializamos negócios</h1>
                 <h4 class="mb-4">Aliados a tecnologia, estruturamos dados e geramos visões que auxiliam na gestão do seu negócio, guiando a transformação de processos e pessoas para alavancar os resultados.</h4>
                 <?php
