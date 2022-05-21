cb8a4fee4821ba46ebe414fe9b13c4ac6c56911b
kingnaldoo <reinaldosn2016@gmail.com>
2022-05-21 17:44:17 -0300

    refactor/home

--- components/card-partners.php
+++ components/card-partners.php
@@ -1,3 +1,3 @@
-<div class="card-partners">
+<div class="card-partners rounded-circle">
     <img class="rounded-circle w-100 h-100" src="<?php echo $imgUrl ?>" alt="<?php echo $partner ?>">
 </div>
