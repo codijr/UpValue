d6525b6b580e45fc7e54c9ceaf4f6d0736dab474
kingnaldoo <reinaldosn2016@gmail.com>
2022-05-27 18:40:39 -0300

    fix card blog again

--- components/card-blog.php
+++ components/card-blog.php
@@ -22,17 +22,19 @@
         </div>
 
         <div class="d-flex flex-column justify-content-between ms-3 pe-4" id="info">
-            <h4 class="<?php echo $color ?>"><?php echo $title ?></h4>
+            <h4 class="<?php echo $color ?> mb-1"><?php echo $title ?></h4>
 
-            <div class="d-flex mt-3 mb-3" id="author">
-                <img class="me-2" src="<?php echo get_template_directory_uri(); ?>/assets/icons/author.svg" alt="Author icone">
+            <div class="mt-1">
+                <div class="d-flex mb-2" id="author">
+                    <img class="me-2" src="<?php echo get_template_directory_uri(); ?>/assets/icons/author.svg" alt="Author icone">
 
-                <p class="body-2 <?php echo $color ?>">Escrito por <?php echo $author ?></p>
-            </div>
+                    <p class="body-2 <?php echo $color ?>">Escrito por <?php echo $author ?></p>
+                </div>
 
-            <div class="d-flex justify-content-between">
-                <p class="overline <?php echo $color ?>"><?php echo $date ?></p>
-                <p class="overline <?php echo $color ?>"><?php echo $readingTime ?></p>
+                <div class="d-flex justify-content-between">
+                    <p class="overline <?php echo $color ?>"><?php echo $date ?></p>
+                    <p class="overline <?php echo $color ?>"><?php echo $readingTime ?></p>
+                </div>
             </div>
         </div>
     </div>
